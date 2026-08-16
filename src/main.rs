9f39d556283898e679cbc67c0c0b670645e8f0d5
Rafael Freire <rafaelfmg72@gmail.com>
2026-08-15 23:13:15 -0300

    Add: command parsing and remove file path constant

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,28 @@ mod context;
 mod cpu;
 mod utils;
 
-
 use crate::context::CH8Context;
-use crate::constants::FILE_PATH;
 use crate::cpu::run;
 
+use clap::Parser;
+use std::path::Path;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(short, long, default_value = "roms/ibm.ch8")]
+    path: String,
+}
+
 pub fn main() {
-    let mut ctx = CH8Context::init(FILE_PATH);
+    let args = Args::parse();
+
+    if !Path::new(&args.path).exists() { 
+        println!("Invalid path to file. {}", args.path);
+        return;
+    }
+
+    let mut ctx = CH8Context::init(&args.path);
 
     run(&mut ctx);
 }
